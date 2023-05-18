a66a895d6161a65b7bd2c423ef5448c4dbe7bc01
Kamil Garbacz <kamil.garbacz@gmail.com>
2023-05-18 12:30:35 +0200

    styling searchbox

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="flex-col flex directionw-1/5 border-2 border-black-600 p-4">
-    <h3 class="text-xl">Search</h3>
+  <div class="flex-col flex directionw-1/5 border-2 border-black-600 p-4 max-h-44 bg-teal-50">
+    <h3 class="text-xl mb-2">Search</h3>
     <div>
       <input
-        class="border-b-2 border-black-600 w-full"
+        class="border-b-2 border-black-600 w-full mb-2"
         type="text"
-        placeholder="search by name"
-      /><input />
+        placeholder="search by name" />
     </div>
-    <div class="mb-2">
+    <div class="mb-5">
       <select name="status" class="border-b-2 border-black-600 w-full">
         <option value="alive">Alive</option>
         <option value="dead">Dead</option>
