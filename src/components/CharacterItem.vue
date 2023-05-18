943cc20d1094c4e1c1ca2101734a2cba1b3b6e4c
Kamil Garbacz <kamil.garbacz@gmail.com>
2023-05-18 12:30:11 +0200

    use characterItem component

---
+++ src/components/CharacterItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex h-32 w-full mb-4 p-2 border bg-amber-50">
+        <img :src="item.image" :alt="item.name" class="mr-3">
+        <ul>
+            <li>
+                Name:
+                <span class="text-2xl">
+                    {{ item.name }}
+                </span>
+            </li>
+            <li>
+                Gender: {{ item.gender }}
+            </li>
+            <li>
+                Species: {{ item.species }}
+            </li>
+            <li>
+                Status: {{ item.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import Character from '@/models/CharacterModel';
+const props = defineProps({
+    item: { type: Object as PropType<Character>, default: () => { return {} } }
+})
+</script>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,26 +1,9 @@
 <template>
-  <div class="w-4/5 border-2 border-black-800 p-4">
+  <div class="w-4/5 border-2 border-black-800 p-4 bg-teal-50">
     <h3 class="text-xl">Characters list</h3>
     <div v-if="charactersList.length > 0">
       <div v-for="item in charactersList" :key="item.id" class="flex">
-        <img :src="item.image" :alt="item.name" class="mr-3">
-        <ul>
-          <li>
-            Name:
-            <span class="text-2xl">
-              {{ item.name }}
-            </span>
-          </li>
-          <li>
-            Gender: {{ item.gender }}
-          </li>
-          <li>
-            Species: {{ item.species }}
-          </li>
-          <li>
-            Status: {{ item.status }}
-          </li>
-        </ul>
+        <CharacterItem :item="item"></CharacterItem>
       </div>
     </div>
     <div v-else>
@@ -29,7 +12,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-
+import CharacterItem from "./CharacterItem.vue";
 import Character from "@/models/CharacterModel";
 import { PropType } from "vue";
 const props = defineProps({
