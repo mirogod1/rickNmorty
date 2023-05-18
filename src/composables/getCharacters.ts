82b013b86508350bdd458f1f15192a8707f3486b
Kamil Garbacz <kamil.garbacz@gmail.com>
2023-05-18 12:29:29 +0200

    getCaracters composable cleaning

--- src/composables/getCharacters.ts
+++ src/composables/getCharacters.ts
@@ -3,10 +3,18 @@ const characters = ref([]);
 const error = ref(null);
 const getCharacters = () => {
   const load = async () => {
+    try {
       const data = await fetch("https://rickandmortyapi.com/api/character");
-      const test = await data.json();
-      characters.value = test.results;
-      console.log(characters.value);
+      if(!data.ok){
+        throw Error("error");
+      }
+      data.json().then(jsonData => {
+        characters.value = jsonData.results;
+      })
+    }
+    catch(e: any){
+      error.value = e.message
+    }
   };
   return { characters, error, load };
 };
