8dff95bd7281366d3cadf8b27920c9c002f24042
Kamil Garbacz <kamil.garbacz@gmail.com>
2023-05-19 14:13:22 +0200

    make searchbox search

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -13,9 +13,14 @@
         <div class="mb-5">
           <select name="status" class="border-b-2 border-black-600 w-full" v-model="statusSearch">
             <option value="" class="text-slate-400" disabled selected>search by status</option>
-            <option v-for="option in options" :key="option">{{ option }}</option>
+            <!-- <option v-for="option in options" :key="option">{{ option }}</option> -->
+            <option>All</option>
+            <option>Alive</option>
+            <option>Dead</option>
+            <option>unknown</option>
           </select>
         </div>
+        <button class="w-2/5 bg-sky-500 text-slate-200 rounded" @click="search()">Search</button>
         <button class="w-2/5 bg-sky-500 text-slate-200 rounded" @click="resetSearch()">Reset</button>
       </div>
       <p>page {{ pageNumber }} of {{ characterPageInfo?.pages }}</p>
@@ -40,27 +45,43 @@ const nameSearch = ref("");
 const statusSearch = ref("");
 
 const searchStore = useSearchStore();
-const { setFilter } = searchStore;
 const { name, status } = storeToRefs(searchStore);
 
 const charactersListStore = useCharactersListStorage();
 const { setListResult } = charactersListStore;
 const { charactersList, error } = storeToRefs(charactersListStore);
 
-watchEffect(() => {
-  setFilter(name.value, status.value);
-});
+const search = () => {
+  const urlParams = new URLSearchParams(characterPageInfo?.value?.next?.split("?")[1]);
+  urlParams.set("page", "1");
+  if (nameSearch.value) {
+    urlParams.set("name", nameSearch.value);
+  }
+  if (statusSearch.value) {
+    if (statusSearch.value == "All") {
+      statusSearch.value = ""
+      urlParams.delete("status")
+    }
+    urlParams.set("status", statusSearch.value);
+  }
+  const newUrl = characterPageInfo?.value?.next?.split("?")[0] + "?" + urlParams.toString();
+  console.log(newUrl);
+
+  loadPage(newUrl)
+}
+
 const resetSearch = () => {
   nameSearch.value = name.value = "";
   statusSearch.value = status.value = "";
+  loadPage();
+
 }
 const pageNumber = computed(() => {
-  const t = new URLSearchParams(characterPageInfo?.value?.next?.split("?")[1]);
-  console.log(t.get("page"));
+  const urlParams = new URLSearchParams(characterPageInfo?.value?.next?.split("?")[1]);
   const sumPages = characterPageInfo?.value?.pages;
-  return t?.get("page") ? Number(t.get("page")) - 1 : sumPages;
+  return urlParams?.get("page") ? Number(urlParams.get("page")) - 1 : sumPages;
 })
-const loadPage = (url: any) => {
+const loadPage = (url?: any) => {
   setListResult(url);
 }
 const characterPageInfo = computed(() => {
